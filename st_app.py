0dfd58e19f27388e5cbf5093ca4395466ddc174d
edsaa <esaavedrac@u.northwestern.edu>
2023-08-29 11:55:23 -0500

    Get if embed

--- st_app.py
+++ st_app.py
@@ -3,19 +3,33 @@ import pyvista as pv
 from stpyvista import stpyvista
 import pickle
 import subprocess
+import urllib.parse as parse
 
-st.set_page_config(page_icon="🧊", layout="wide")
+def is_embed():
+    from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+    ctx = get_script_run_ctx()
+    query_params = parse.parse_qs(ctx.query_string)
+    return query_params.get("embed", False)[0]
+
+APP_EMBED = is_embed()
+
+st.set_page_config(
+    page_title="stpyvista",
+    page_icon="🧊", 
+    layout="wide" if APP_EMBED else "centered", 
+    initial_sidebar_state="collapsed" if APP_EMBED else "expanded")
 
 ## Check if xvfb is already running on the machine
 is_xvfb_running = subprocess.run(["pgrep", "Xvfb"], capture_output=True)
 
 if is_xvfb_running.returncode == 1:
-    with st.sidebar:
-        st.warning("Xvfb was not running...")
+    if not APP_EMBED:
+        st.toast("Xvfb was not running...", icon="⚠️")
     pv.start_xvfb()
 else:
-    with st.sidebar:
-        st.info(f"Xvfb is running! \n\n`PID: {is_xvfb_running.stdout.decode('utf-8')}`")
+    if not APP_EMBED:
+        st.toast(f"Xvfb is running! \n\n`PID: {is_xvfb_running.stdout.decode('utf-8')}`", icon="📺")
 
 # @st.cache_data
 # def get_cow(does_nothing="cow"):
@@ -29,53 +43,60 @@ with open("assets/style.css") as f:
     st.markdown(f"""<style>{f.read()}</style>""", unsafe_allow_html=True)
 
 # Add badges to sidebar
-with st.sidebar:
-    with open("assets/badges.md") as f:
-        st.markdown(f"""{f.read()}""", unsafe_allow_html=True)
+if not APP_EMBED:
+    with st.sidebar:
+        with open("assets/badges.md") as f:
+            st.markdown(f"""{f.read()}""", unsafe_allow_html=True)
 
 st.title("🧊 `stpyvista`")
-st.header("Show PyVista 3D visualizations in Streamlit")
+st.subheader("Show PyVista 3D visualizations in Streamlit")
 
-cols = st.columns([1, 1])
+## Pyvista code
+cow = pv.Cylinder(radius=3.5, height=8)
+nose = pv.Cylinder(radius=0.5, height=8, direction=(0, 0, 1), center=(0, 0, 1.7))
+eye_left = pv.Cylinder(radius=1.0, height=4, direction=(0, 0, 1), center=(-2.0, 1, 2))
+eye_left_p = pv.Cylinder(radius=0.3, height=4.1, direction=(0, 0, 1), center=(-2.0, 1, 2))
+eye_right = pv.Cylinder(radius=1.0, height=4, direction=(0, 0, 1), center=(2.0, 1, 2))
+eye_right_p = pv.Cylinder(radius=0.3, height=4.1, direction=(0, 0, 1), center=(2.0, 1, 2))
 
-with cols[0]:
-    st.markdown(
-        """
-    `stpyvista` is a simple component that takes a PyVista plotter object and shows 
-    it on Streamlit as an interactive element (as in it can be zoomed in/out, moved 
-    and rotated, but the visualization state is not returned).
-    It uses PyVista's [panel backend](https://docs.pyvista.org/user-guide/jupyter/panel.html) 
-    for taking the plotter, [exports it to HTML](https://panel.holoviz.org/reference/panes/VTK.html) 
-    and displays that within an iframe.
 
-    **👈 Check the list of demos**
-    ****
-    """,
-        unsafe_allow_html=True,
-    )
+plotter = pv.Plotter()
 
-with cols[1]:
-    cow = pv.Cylinder(radius=3.5, height=8)
+plotter.add_mesh(cow, color="grey", pbr=True, metallic=0.05)
+plotter.add_mesh(nose, color="red", pbr=True, metallic=0.05)
+plotter.add_mesh(eye_left, color="white", pbr=True, metallic=0.05)
+plotter.add_mesh(eye_right, color="white", pbr=True, metallic=0.05)
+plotter.add_mesh(eye_left_p, color="green")
+plotter.add_mesh(eye_right_p, color="green")
 
-    plotter = pv.Plotter()
 
-    plotter.add_mesh(cow, color="pink", pbr=True, metallic=0.05)
+plane = pv.Plane(center=[0, -3.65, 0], direction=[0, 1, 0], i_size=12, j_size=12)
+plotter.add_mesh(plane, color="#09ab3b", show_edges=True)
 
-    plane = pv.Plane(center=[0, -3.65, 0], direction=[0, 1, 0], i_size=12, j_size=12)
-    # plane.point_data.clear()
-    plotter.add_mesh(plane, color="#09ab3b", show_edges=True)
-    # plotter.add_floor('-y', show_edges=True, color="#09ab3b", opacity=0.8)
+# plotter.add_floor('-y', show_edges=True, color="#09ab3b", opacity=0.8)
 
-    ## Send to streamlit
-    plotter.background_color = "white"
-    plotter.camera.zoom("tight")
-    plotter.view_xy()
-    plotter.camera.azimuth = 45
-    plotter.camera.elevation = 25
-    plotter.window_size = [350, 300]
-    stpyvista(plotter, horizontal_align="left",
-        panel_kwargs=dict(orientation_widget=True, interactive_orientation_widget=True)
-    )
+## Send to streamlit
+plotter.background_color = "white"
+plotter.view_xy()
+plotter.camera.azimuth = 25
+plotter.camera.elevation = 15
+
+plotter.window_size = [450, 300]
+stpyvista(plotter, horizontal_align="center",
+    panel_kwargs=dict(orientation_widget=True, interactive_orientation_widget=True)
+)
+
+"""
+`stpyvista` is a simple component that takes a PyVista plotter object and shows 
+it on Streamlit as an interactive element (as in it can be zoomed in/out, moved 
+and rotated, but the visualization state is not returned).
+It uses PyVista's [panel backend](https://docs.pyvista.org/user-guide/jupyter/panel.html) 
+for taking the plotter, [exports it to HTML](https://panel.holoviz.org/reference/panes/VTK.html) 
+and displays that within an iframe.
+
+**👈 Check the list of demos**
+****
+"""
 
 with st.expander("🛠️ Installation"):
     """
