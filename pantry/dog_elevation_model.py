660da1efc867db1908ea695745a24911eb0970d2
edsaa <esaavedrac@u.northwestern.edu>
2024-04-12 15:24:37 -0500

    test wrap xvfb as cached resource

--- pantry/dog_elevation_model.py
+++ pantry/dog_elevation_model.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import streamlit as st
 from pyvista.plotting.utilities import start_xvfb
 from stpyvista import stpyvista
@@ -7,13 +9,23 @@ from stpyvista.utils import is_the_app_embedded
 from stpyvista_pantry import dog_texture
 
 # Initial configuration
-start_xvfb()
-st.session_state.is_app_embedded = st.session_state.get(
-    "is_app_embedded", is_the_app_embedded()
-)
+@st.cache_resource
+def initial_config(dummy:str = "init"):
+    start_xvfb()
+    return
+
+@st.cache_resource
+def get_css(dummy:str = "css"):
+    css_list = []
+    for style_sheet in ["./assets/style.css", "./assets/style_embed.css"]:
+        with open(style_sheet) as f:
+            css_list.append(f.read())
+
+    return css_list
 
 
 def main():
+    
     st.set_page_config(
         page_title="stpyvista: Dog Canyon",
         page_icon="🐕",
@@ -21,16 +33,37 @@ def main():
         initial_sidebar_state="collapsed",
     )
 
-    # Add some styling with CSS selectors
-    for style_sheet in ["./assets/style.css", "./assets/style_embed.css"]:
-        with open(style_sheet) as f:
-            st.markdown(f"""<style>{f.read()}</style>""", unsafe_allow_html=True)
+    initial_config()
+
+    st.session_state.is_app_embedded = st.session_state.get(
+        "is_app_embedded", is_the_app_embedded()
+    )
     
-    # Load pyvista Plotter`
+    # Add some styling with CSS selectors
+    for css in get_css():
+        st.html(f"<style>{css}</style>")
+    
+    # Load pyvista Plotter
     dog = dog_texture()
 
     if not st.session_state.is_app_embedded:
-        st.markdown(
+        
+        st.header("🐕   Dog Elevation Model (DEM)", divider="rainbow")
+        "&nbsp;"
+
+        cols = st.columns([1, 3])
+        
+        with cols[0]:
+            "Generate a digital elevation model from an image's brightness."
+            st.image("./assets/img/gloria_pickle.jpg")
+            st.caption(
+                "Gloria from [The Coding Train](https://thecodingtrain.com/challenges/181-image-stippling)"
+            )
+        
+        with cols[1]:
+            container_3d = st.container()
+
+        st.html(
             """
             <style>
                 p{
@@ -45,25 +78,9 @@ def main():
                     width: max(25%, 12vw);
                 }
             </style>
-            """,
-            unsafe_allow_html=True,
+            """
         )
         
-        st.header("🐕   Dog Elevation Model (DEM)", divider="rainbow")
-        "&nbsp;"
-
-        cols = st.columns([1, 3])
-
-        with cols[0]:
-            "Generate a digital elevation model from an image's brightness."
-            st.image("./assets/img/gloria_pickle.jpg")
-            st.caption(
-                "Gloria from [The Coding Train](https://thecodingtrain.com/challenges/181-image-stippling)"
-            )
-
-        with cols[1]:
-            container_3d = st.container()
-
     else:
         container_3d = st.container()
 
@@ -73,11 +90,13 @@ def main():
             panel_kwargs=dict(
                 orientation_widget=True, interactive_orientation_widget=True
             ),
+            bokeh_resources="CDN",
             use_container_width=True,
+            key="dem-dog"
         )
 
     if st.session_state.is_app_embedded:
-        st.markdown(
+        st.html(
             """
             <style>
                 body{
@@ -96,10 +115,8 @@ def main():
                     transform: translateY(-12px);
                 }
             </style>
-            """,
-            unsafe_allow_html=True,
+            """
         )
-
 
 if __name__ == "__main__":
     main()
